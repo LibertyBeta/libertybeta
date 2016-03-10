4311a699c820ff7db140b3acb19c20901c728f4e
Rainer Paskiewicz <rainer.paskiewicz@gmail.com>
2016-03-10 14:05:25 -0500

    Mobile Pesrona Tweaking
    This is the final set of the mobile persona tweak. We've tuned the
    waterfall header, we've added a contact section on the top of the page,
    and we've adjusted the shadow cutoff.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
+
+
   var waterfallDisplay = document.getElementsByClassName('waterfall')[0];
   var totalWaterfallHieght = waterfallDisplay.clientHeight;
   var nav = document.getElementsByTagName('nav')[0];
-  var shadowMoment = totalWaterfallHieght * 0.75;
+  var shadowMoment = totalWaterfallHieght * 0.90;
+
+
   if(waterfallDisplay.getBoundingClientRect.bottom <= 0 ){
     nav.classList.add("active");
   }
 
 
   window.onscroll = function waterfallHandler(){
-    console.log("checking the height of the display for visibility");
-    console.log(waterfallDisplay.getBoundingClientRect().bottom);
-    console.log(shadowMoment);
     if(waterfallDisplay.getBoundingClientRect().bottom <= 0 ){
       nav.classList.add("active");
-      console.log("Waterfall is off screen");
       return true;
     }
-    // console.log(waterfallDisplay.bottom);
     if(waterfallDisplay.getBoundingClientRect().bottom < shadowMoment){
       nav.classList.add("active");
     } else {
